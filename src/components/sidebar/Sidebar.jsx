95ae9321e32f4e983e3674cb8818abd128e9f02b
Mia Azevedo <mialuzazevedo@gmail.com>
2023-10-13 18:20:39 -0400

    fixed the burguer menu so that it closes when a button is clicked

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -9,20 +9,31 @@ import { useLogout } from '../../hooks/useLogout'
 // components
 import { slide as Menu } from 'react-burger-menu'
 import Button from '../button/Button'
+import { useState } from 'react';
 
 
 const Sidebar = () => {
   const { user } = useAuthContext()
   const { logout, isPending } = useLogout()
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleIsMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+
+  }
 
   return (
-    <Menu right >
-      <Link to='/' className="menu-item">Home</Link>
-      <Link to='/devs' className="menu-item">Devs</Link>
-      {user && <Link to='/profile' className="menu-item">Profile</Link>}
+    <Menu right isOpen={isMenuOpen} onOpen={handleIsMenuOpen} onClose={handleIsMenuOpen}>
+      <Link to='/' className="menu-item" onClick={closeMenu}>Home</Link>
+      <Link to='/devs' className="menu-item" onClick={closeMenu}>Devs</Link>
+      {user && <Link to='/profile' className="menu-item" onClick={closeMenu}>Profile</Link>}
 
       {!user && 
-        <Link to='/signup'>
+        <Link to='/signup' onClick={closeMenu}>
           <Button className="menu-button" text='Cadastro'/>
         </Link>}
 
@@ -31,7 +42,10 @@ const Sidebar = () => {
           {!isPending &&<Button 
             text='Logout' 
             className="menu-button" 
-            onClick={logout}/>
+            onClick={() => {
+              logout()
+              closeMenu()
+            }}/>
           }
           {isPending &&<Button 
             text='Login out...' 
